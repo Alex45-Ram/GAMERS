a53c662301738f799e90a3904f49adb0d45eb481
jesus barroso <jjbarrocito@gmail.com>
2024-10-21 19:23:55 -0600

    accesrorios

--- Accesorios.Designer.cs
+++ Accesorios.Designer.cs
@@ -32,7 +32,19 @@
             button1 = new Button();
             label1 = new Label();
             pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -42,9 +54,10 @@
             button1.Cursor = Cursors.Hand;
             button1.Font = new Font("Lucida Handwriting", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.ForeColor = Color.Black;
-            button1.Location = new Point(-3, 559);
+            button1.Location = new Point(-5, 931);
+            button1.Margin = new Padding(5, 5, 5, 5);
             button1.Name = "button1";
-            button1.Size = new Size(216, 61);
+            button1.Size = new Size(351, 98);
             button1.TabIndex = 2;
             button1.Text = "Volver";
             button1.UseVisualStyleBackColor = false;
@@ -56,10 +69,11 @@
             label1.BackColor = Color.Transparent;
             label1.BorderStyle = BorderStyle.FixedSingle;
             label1.Font = new Font("Snap ITC", 19.8000011F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.Blue;
-            label1.Location = new Point(663, 9);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(1077, 14);
+            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(228, 46);
+            label1.Size = new Size(355, 71);
             label1.TabIndex = 3;
             label1.Text = "Accesorios";
             label1.TextAlign = ContentAlignment.TopCenter;
@@ -68,26 +82,95 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-3, -1);
+            pictureBox1.Location = new Point(-5, -2);
+            pictureBox1.Margin = new Padding(5, 5, 5, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(387, 607);
+            pictureBox1.Size = new Size(678, 1031);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
             // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(709, 211);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(282, 241);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(1093, 211);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(499, 241);
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(709, 577);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(282, 241);
+            pictureBox4.TabIndex = 7;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(1685, 211);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(282, 241);
+            pictureBox5.TabIndex = 8;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(1222, 577);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(282, 241);
+            pictureBox6.TabIndex = 9;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(1685, 577);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(282, 241);
+            pictureBox7.TabIndex = 10;
+            pictureBox7.TabStop = false;
+            // 
             // Accesorios
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1240, 617);
+            ClientSize = new Size(2015, 1027);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
             Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
+            Margin = new Padding(5, 5, 5, 5);
             Name = "Accesorios";
             Text = "Accesorios";
             Load += Accesorios_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -97,5 +180,11 @@
         private Button button1;
         private Label label1;
         private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
     }
 }
