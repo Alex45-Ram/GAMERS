c404b97d527eea9b0607c11dc0e1a50a89b7203c
angelzk 🎮🎮 <angelmartin23mayo@gmail.com>
2024-11-25 20:20:46 -0600

    Se agrego ticket

---
+++ Efectivo.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace GAMERS
+{
+    partial class Efectivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Efectivo));
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            imageList1 = new ImageList(components);
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ControlDarkDark;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-4, -8);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1979, 991);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
+            pictureBox1.Tag = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Black;
+            label2.Font = new Font("Stencil", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.Control;
+            label2.Location = new Point(605, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(755, 71);
+            label2.TabIndex = 19;
+            label2.Text = "MEDIO DE PAGO EFECTIVO";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Black;
+            label3.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(446, 189);
+            label3.Name = "label3";
+            label3.Size = new Size(274, 33);
+            label3.TabIndex = 20;
+            label3.Text = "TOTAL DE LA DEUDA";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Black;
+            label1.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(446, 307);
+            label1.Name = "label1";
+            label1.Size = new Size(332, 33);
+            label1.TabIndex = 21;
+            label1.Text = "CANTIDAD DE EFECTIVO:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Black;
+            label4.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.Control;
+            label4.Location = new Point(446, 601);
+            label4.Name = "label4";
+            label4.Size = new Size(127, 33);
+            label4.TabIndex = 22;
+            label4.Text = "CAMBIO:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(841, 467);
+            button1.Name = "button1";
+            button1.Size = new Size(288, 63);
+            button1.TabIndex = 23;
+            button1.Text = "PAGAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(799, 198);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 20);
+            label5.TabIndex = 24;
+            label5.Click += label5_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(745, 191);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(240, 27);
+            textBox1.TabIndex = 25;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(821, 313);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(240, 27);
+            textBox2.TabIndex = 26;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(637, 601);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(240, 27);
+            textBox3.TabIndex = 27;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Black;
+            label6.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.Control;
+            label6.Location = new Point(735, 422);
+            label6.Name = "label6";
+            label6.Size = new Size(332, 33);
+            label6.TabIndex = 28;
+            label6.Text = "CANTIDAD DE EFECTIVO:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Black;
+            label7.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.Control;
+            label7.Location = new Point(661, 744);
+            label7.Name = "label7";
+            label7.Size = new Size(632, 33);
+            label7.TabIndex = 29;
+            label7.Text = "GRACIAS POR PREFERIRNOS, VUELVA PRONTO.";
+            // 
+            // Efectivo
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1802, 876);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Name = "Efectivo";
+            Text = "Efectivo";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label1;
+        private Label label4;
+        private Button button1;
+        private ImageList imageList1;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label6;
+        private Label label7;
+    }
+}
---
+++ Tarjeta.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace GAMERS
+{
+    partial class Tarjeta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tarjeta));
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            label7 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ControlDarkDark;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-88, -57);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1979, 991);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 14;
+            pictureBox1.TabStop = false;
+            pictureBox1.Tag = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Black;
+            label2.Font = new Font("Stencil", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.Control;
+            label2.Location = new Point(485, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(737, 71);
+            label2.TabIndex = 18;
+            label2.Text = "MEDIO DE PAGO TARJETA";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Black;
+            label3.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(206, 198);
+            label3.Name = "label3";
+            label3.Size = new Size(255, 33);
+            label3.TabIndex = 19;
+            label3.Text = "NUMERO TARJETA";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Black;
+            label1.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(206, 286);
+            label1.Name = "label1";
+            label1.Size = new Size(308, 33);
+            label1.TabIndex = 20;
+            label1.Text = "NOMBRE DEL TITULAR";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Black;
+            label4.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.Control;
+            label4.Location = new Point(206, 373);
+            label4.Name = "label4";
+            label4.Size = new Size(331, 33);
+            label4.TabIndex = 21;
+            label4.Text = "FECHA DE VENCIMIENTO";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Black;
+            label5.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.Control;
+            label5.Location = new Point(206, 447);
+            label5.Name = "label5";
+            label5.Size = new Size(65, 33);
+            label5.TabIndex = 22;
+            label5.Text = "CVV";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(558, 198);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(274, 27);
+            textBox1.TabIndex = 23;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(558, 292);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(274, 27);
+            textBox2.TabIndex = 24;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(558, 373);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(274, 27);
+            textBox3.TabIndex = 25;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(351, 447);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(130, 27);
+            textBox4.TabIndex = 26;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1133, 487);
+            button1.Name = "button1";
+            button1.Size = new Size(211, 71);
+            button1.TabIndex = 27;
+            button1.Text = "PAGAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Black;
+            label7.Font = new Font("Stencil", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.Control;
+            label7.Location = new Point(467, 732);
+            label7.Name = "label7";
+            label7.Size = new Size(632, 33);
+            label7.TabIndex = 30;
+            label7.Text = "GRACIAS POR PREFERIRNOS, VUELVA PRONTO.";
+            // 
+            // Tarjeta
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1802, 876);
+            Controls.Add(label7);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Name = "Tarjeta";
+            Text = "Tarjeta";
+            Load += Tarjeta_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label1;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Label label7;
+    }
+}
